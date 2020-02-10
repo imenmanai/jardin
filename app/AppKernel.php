602a80e21edab8fdf9fe7d6ace1ac8c1ffd5acb0
imenmanai <48943828+imenmanai@users.noreply.github.com>
2020-02-10 19:33:19 +0100

    imencommit1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new mainBundle\mainBundle(),
             new coursBundle\coursBundle(),
+            new platBundle\platBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
