ec4b52d1fe2c709d13fc097cbfbe340e74bab496
houria-hedhili <48753466+houria-hedhili@users.noreply.github.com>
2020-02-10 20:08:26 +0100

    houria2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new mainBundle\mainBundle(),
             new coursBundle\coursBundle(),
+            new evenementBundle\evenementBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
