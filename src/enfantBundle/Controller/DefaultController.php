bdf98b5ab859a3dedc0f563a0cd60671d8b05fc4
wifeko <wifek.ouerghemmi@esprit.tn>
2020-02-11 11:21:52 +0100

    wifekAffichage

--- src/enfantBundle/Controller/DefaultController.php
+++ src/enfantBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace enfantBundle\Controller;
 
+use enfantBundle\Entity\Bus;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -9,5 +10,10 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('enfantBundle:Default:index.html.twig');
+    }
+    public function afficherBusAction()
+    {
+        $buss=$this->getDoctrine()->getRepository(Bus::class)->findAll();
+        return $this->render('@enfant/Default/afficherBus.html.twig',array('buss'=>$buss));
     }
 }
