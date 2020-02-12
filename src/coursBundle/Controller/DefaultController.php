7ba0027b12d177c0d67238f92613c31b64cee85e
houria-hedhili <48753466+houria-hedhili@users.noreply.github.com>
2020-02-12 16:37:28 +0100

    crudavantfinalhou

--- src/coursBundle/Controller/DefaultController.php
+++ src/coursBundle/Controller/DefaultController.php
@@ -3,7 +3,12 @@
 namespace coursBundle\Controller;
 
 use coursBundle\Entity\Cours;
+use coursBundle\Entity\Matiere;
+use coursBundle\Form\CoursmodifType;
+use coursBundle\Form\CoursType;
+use coursBundle\Form\MatiereType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -18,20 +23,69 @@ class DefaultController extends Controller
         return $this->render('@cours/view/AfficherCours.html.twig',array('cours'=>$courss));
 
     }
-    public function AjouterCoursBAction($id,Request $request)
-    {
-        $c=$this->getDoctrine()->getManager();//responsqble pour la communication avec la base donne //getmanaager de permet d appeler les fomction remove flush..
-        $pass=$this->getDoctrine()->getRepository(Cours::class)->find($id);
-        if($request->isMethod('POST')) {
-            $ref = $request->get('ref');
-            $pass->setNbe_bagage($pass->getNbe_bagage()+$ref);
-            $pass->setTotal_frais($pass->getTotal_frais()+(50*$pass->getNbe_bagage())) ;
-            $c->persist($pass);
-            $c->flush();
-            return $this->redirectToRoute('afficher');
-        }return $this->render("@GestionVol/Default/bagage.html.twig");
+
+    public function ajoutercatAction(Request $request){
+        $matiere= new Matiere();
+        $form=$this->createForm(MatiereType::class,$matiere);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($matiere);//persister les donner dans la base de donnee
+            $em->flush();//tlansi kif el commit
+            return $this->redirectToRoute('Affichcatcours');
+        }
+        return $this->render('@cours/view/ajouterCategorie.html.twig', array('form' => $form->createView()));
+
     }
-    public function ajoutercatAction(){
-        return $this->render('@cours/view/ajouterCategorie.html.twig');
+    public function AjouterCoursAction(Request $request){
+        $cours= new Cours();
+        $form=$this->createForm(CoursType::class,$cours);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($cours);
+            $em->flush();
+            return $this->redirectToRoute('AfficherCours');
+        }
+        return $this->render('@cours/view/ajouterCours.html.twig', array('form' => $form->createView()));
+
+    }
+    public function affichercategorieAction(){
+
+        $matieres=$this->getDoctrine()->getRepository(Matiere::class)->findAll();
+        return $this->render('@cours/view/AfficherCategorie.html.twig',array('matiere'=>$matieres));
+    }
+    public function supprimercategorieAction(Request $request,$id){
+
+        $model=$this->getDoctrine()->getManager();
+        $modeleasupp=$this->getDoctrine()->getRepository(Matiere::class)->find($id);
+        $model->remove($modeleasupp);
+        $model->flush();
+        return $this->redirectToRoute("Affichcatcours");
+    }
+    public function supprimercoursAction(Request $request,$id){
+        $cours=$this->getDoctrine()->getManager();
+        $coursasupp=$this->getDoctrine()->getRepository(Cours::class)->find($id);
+        $cours->remove($coursasupp);
+        $cours->flush();
+        return $this->redirectToRoute("AfficherCours");
+    }
+    public function modifiercoursAction(Request $request,$id){
+        $cours= new Cours();
+        $em=$this->getDoctrine()->getManager();
+        $cours=$em->getRepository(Cours::class)->find($id);
+        $form=$this->createForm(CoursmodifType::class,$cours);
+        $form->handleRequest($request);
+        if($form->isSubmitted()) {
+            $em=$this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('AfficherCours');
+        }
+
+        return $this->render('@cours/view/modifiercours.html.twig', array('form' => $form->createView()));
+
     }
 }
---
+++ src/evenementBundle/Form/EventmodifType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace evenementBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventmodifType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('description')->add('date')->add('nbpart')->add('nom')->add('local')->add('image','Symfony\Component\Form\Extension\Core\Type\FileType',array('data_class' => null));
+        ;
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'evenementBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'evenementbundle_event';
+    }
+
+
+}
