6fdf03a11d95ddca0f28acaa1c3c346906ce8759
imenmanai <48943828+imenmanai@users.noreply.github.com>
2020-02-12 08:58:37 +0100

    imenCRUDv1

---
+++ src/platBundle/Entity/plat.php
@@ -0,0 +1,121 @@
+<?php
+
+
+namespace platBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *@ORM\Entity()
+
+ */
+class plat
+{
+    /**
+     *@ORM\Column(type="integer")
+     *@ORM\Id
+     *@ORM\GeneratedValue(strategy="AUTO")
+     */
+private $id;
+    /**
+     * @ORM\Column(type="string",length=255)
+     */
+private $nom;
+    /**
+     * @ORM\Column(type="string",length=255)
+     */
+private $description;
+    /**
+     * @ORM\Column(type="string",length=255)
+     */
+private $image;
+    /**
+     * @ORM\Column(type="date")
+     */
+private $date;
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+
+
+
+
+}
